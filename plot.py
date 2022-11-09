c3918af20d833695c98e22297b204cc129737e55
Zemke <florian@zemke.io>
2022-11-09 17:23:15 +0100

    Init

---
+++ plot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3 
+
+import matplotlib.pyplot as plt
+import math
+
+plt.rcParams['text.usetex'] = True
+
+xx = range(1, 100)
+for a in [50, 30, 20, 5]:
+  yy = [max(min(round(-1+math.log10(x*.13)*a),21),1) for x in xx]
+  plt.plot(xx, yy, label=f'a={a}')
+
+plt.ylabel('relativization steps S')
+plt.xlabel('max number of games played of any user')
+plt.legend()
+plt.title(r'$S = \max(\min(\left\lfloor -1+\log(x*.13)*a \right\rceil,21),1)$')
+plt.show()
+plt.savefig('images/relsteps.png')
+
