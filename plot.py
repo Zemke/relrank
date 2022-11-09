282b58124e8ac39c3c82b9f31f2b374761198606
Zemke <florian@zemke.io>
2022-11-09 18:03:05 +0100

    fix

--- plot.py
+++ plot.py
@@ -15,7 +15,7 @@ plt.ylabel('relativization steps S')
 plt.xlabel('max number of games played of any user')
 plt.legend()
 plt.title(r'$S = \max(\min(\left\lfloor -1+\log(x*.13)*a \right\rceil,21),1)$')
-if 'show' == sys.argv[1]:
+if len(sys.argv) >= 2 and 'show' == sys.argv[1]:
   plt.show()
 else:
   plt.savefig('images/relsteps.png')
