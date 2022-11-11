9877c1b15368d623e21d3f447913f1d0ddd81637
Zemke <florian@zemke.io>
2022-11-11 16:15:03 +0100

    Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zemke/relrank
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func HelloWorld(name string) string {
+  return "Hello, " + name
+}
+
+func main() {
+    fmt.Println(HelloWorld("Flori"))
+}
+
