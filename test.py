26a3c7a089851c723db3736e9c598cb8d141fece
Zemke <florian@zemke.io>
2022-11-21 16:47:28 +0100

    DEBUG, MAE, RMSE

--- test.py
+++ test.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python3
 
 import subprocess as sp
+import math
+import os
 
 U = {i: n for i,n in [l.split(',') for l in open('csv/users.csv', 'r').read().splitlines()]}
 
-for n in [38,39,40,41]:
+debug = os.getenv('DEBUG', '0')
+print('env', env := f"DEBUG={debug} RELRANK_ROUND=2")
+nn = [38,39,40,41]
+maes, rmses = 0, 0
+for n in nn:
   print('season', n)
+  # TODO skip users with no won rounds
   R = {u: r for u,r in [l.split(',') for l in open(f'csv/{n}ranking.csv', 'r').read().splitlines()]}
   mx = max([float(r) for _, r in list(R.items())[1:]])
-  cmd = f'DEBUG=0 RELRANK_ROUND=2 RELRANK_SCALE_MAX={mx} go run . < csv/{n}games.csv'
-  print('cmd', cmd)
+  cmd = f'{env} RELRANK_SCALE_MAX={mx} go run . < csv/{n}games.csv'
   out = sp.run(cmd, shell=True, capture_output=True, text=True)
-  print(out.stderr)
   urr = out.stdout.splitlines()
   F = []
   pad = 0
   for l in urr:
     u,r = l.split(',')
+    if u not in R:
+      continue
     pad = max(pad, len(r))
-    F.append((float(r), U[u]))
+    F.append((float(r), float(R[u]), U[u]))
   F.sort()
-  for r,u in F[::-1]:
-    print(f"{r:6.2f},{u}")
+  dd = []
+  for rn,ro,u in F[::-1]:
+    if debug == '1':
+      print(f"{rn:6.2f},{ro:6.2f},{u}")
+    dd.append(rn - ro)
+  mae = sum([abs(dd[i-1] - dd[i]) for i in range(1, len(dd))]) / len(dd)
+  print(n, 'MAE', mae)
+  rmse = math.sqrt(sum([(dd[i-1] - dd[i])**2 for i in range(1, len(dd))]) / len(dd))
+  print(n, 'RMSE', rmse)
+  maes += mae
+  rmses += rmse
+
+print('average')
+print('MAE', maes / len(nn))
+print('RMSE', rmses / len(nn))
 
