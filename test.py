c7db21a771c2c1877793dc07151406ca78021e1f
Zemke <florian@zemke.io>
2022-11-22 16:14:28 +0100

    min games and skip seasons

--- test.py
+++ test.py
@@ -3,26 +3,45 @@
 import subprocess as sp
 import math
 import os
+import sys
+from collections import Counter
 
 U = {i: n for i,n in [l.split(',') for l in open('csv/users.csv', 'r').read().splitlines()]}
 
 debug = os.getenv('DEBUG', '0')
+minw = int(os.getenv('MINW', '5'))
+print('minw', minw)
 print('env', env := f"DEBUG={debug} RELRANK_ROUND=2")
-nn = [38,39,40,41]
+nn = [int(sys.argv[1])] if len(sys.argv) > 1 else [38,39,40,41]
 maes, rmses = 0, 0
 for n in nn:
   print('season', n)
-  # TODO skip users with no won rounds
+  C = Counter()
+  for l in open(f"csv/{n}games.csv", 'r').read().splitlines()[1:]:
+    hi,ai,hsc,asc = l.split(',')
+    C[hi] += int(hsc)
+    C[ai] += int(asc)
+  uws = [str(u) for u,w in C.items() if w >= minw]
   R = {u: r for u,r in [l.split(',') for l in open(f'csv/{n}ranking.csv', 'r').read().splitlines()]}
   mx = max([float(r) for _, r in list(R.items())[1:]])
   cmd = f'{env} RELRANK_SCALE_MAX={mx} go run . < csv/{n}games.csv'
+  if debug == '1':
+    print('cmd', cmd)
   out = sp.run(cmd, shell=True, capture_output=True, text=True)
   urr = out.stdout.splitlines()
+  if debug == '1':
+    print('stderr')
+    print(out.stderr)
+  if debug == '1':
+    print('stdout')
+    print(out.stdout)
   F = []
   pad = 0
   for l in urr:
     u,r = l.split(',')
-    if u not in R:
+    if u not in R or u not in uws:
+      if debug == '1':
+        print(f"skipping {u} ({U[u]})")
       continue
     pad = max(pad, len(r))
     F.append((float(r), float(R[u]), U[u]))
